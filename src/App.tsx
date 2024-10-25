1378592564755780ff1a65b97dc1b8cbbd83a0f9
Artem <universalism123@gmail.com>
2024-10-26 00:47:55 +0300

    add MUI + add SCSS + add fonts = setup has completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div>
-      Hello World!
+      <h1 className='chad-h'>Chad</h1>
+      <h3 className='text'>Connect your Shopify store</h3>
+      <Button variant='contained'>Hello World!</Button>
     </div>
   );
 }
